141debb84b79dc4e22cd9c5d47f7ac1fb7019622
Sergiyan92 <sergiyan@ukr.net>
2023-09-29 11:05:33 +0300

    Initial commit

---
+++ app.ts
@@ -0,0 +1,14 @@
+// const button = document.querySelector("button")! as HTMLButtonElement;
+// const input1 = document.getElementById("number1")! as HTMLInputElement;
+// const input2 = document.getElementById("number2")! as HTMLInputElement;
+
+// function add(number1:number, number2:number) {
+//   return number1 + number2;
+// }
+
+// button.addEventListener("click", function() {
+//   console.log(add(+input1.value, +input2.value))
+// })
+let num: number;
+
+num= "hjih"
