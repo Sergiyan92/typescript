d779574e0efcebbdb0a7b609e12a635e9dad4010
Sergiyan92 <sergiyan@ukr.net>
2023-10-02 11:39:22 +0300

    create project

---
+++ src/app.ts
@@ -0,0 +1,45 @@
+type User = {
+  name: string;
+  age: number | string;
+};
+type evetLoop = 'lesson' | 'gad';
+// const button = document.querySelector("button")! as HTMLButtonElement;
+// const input1 = document.getElementById("number1")! as HTMLInputElement;
+// const input2 = document.getElementById("number2")! as HTMLInputElement;
+
+// function add(number1:number, number2:number) {
+//   return number1 + number2;
+// }
+
+// button.addEventListener("click", function() {
+//   console.log(add(+input1.value, +input2.value))
+// })
+// const total: number = 100
+// let empty: null = null
+
+// const numbers: number[] = [1,2,3,4, 5,6]
+
+// const user:{name:string, age:number} = {
+//     name: "Serhii",
+//     age:12
+// }
+// const user: User = {
+//   name: 'serg',
+//   age: 12,
+// };
+// user.age = '15';
+
+// const event: evetLoop = 'gad';
+// let age: any = 10;
+// age = 'bob';
+// age = true;
+
+// let name: unknown = 'gari';
+// name = 12;
+// name.toFixed();
+
+enum Sizes {
+  small = 'small',
+  large = 'large',
+  medium = 'medium',
+}
